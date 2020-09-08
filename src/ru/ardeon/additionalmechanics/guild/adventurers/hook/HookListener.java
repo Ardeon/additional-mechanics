53fc7f3ede4b22c2e174fef6e3299c26173859a4
Builder <89637210314@mail.ru>
2020-09-08 12:51:47 +0300

    delete lead drop from hook

--- src/ru/ardeon/additionalmechanics/guild/adventurers/hook/HookListener.java
+++ src/ru/ardeon/additionalmechanics/guild/adventurers/hook/HookListener.java
@@ -1,17 +1,23 @@
 package ru.ardeon.additionalmechanics.guild.adventurers.hook;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDropItemEvent;
 import org.bukkit.event.entity.EntityUnleashEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.projectiles.ProjectileSource;
 import org.bukkit.util.Vector;
 
@@ -41,19 +47,21 @@ public class HookListener implements Listener {
 				if (w.equals(pLoc.getWorld())&&hookVec.distance(pVec)>5) {
 					Vector direction = pVec.clone().subtract(hookVec).normalize();
 					Entity lastent = projectile;
+					PotionEffect ef = new PotionEffect(PotionEffectType.INVISIBILITY, Integer.MAX_VALUE, 2);
 					int i = 0;
-					while (hookVec.clone().add(direction.clone().multiply(i)).distance(pVec)>5) {
+					while (hookVec.clone().add(direction.clone().multiply(2*i)).distance(pVec)>5) {
 						LivingEntity ent = (LivingEntity)projectile
 								.getWorld()
 								.spawnEntity(projectile.getLocation()
-								.add(direction.clone().multiply(3*i)), 
+								.add(direction.clone().multiply(2*i)), 
 								EntityType.SILVERFISH);
 						ent.setAI(false);
 						ent.setSilent(true);
+						ef.apply(ent);
 						ent.setLeashHolder(lastent);
 						lastent = ent;
 						i++;
-						if (i>20)
+						if (i>40)
 							break;
 					}
 				}
@@ -65,5 +73,25 @@ public class HookListener implements Listener {
 		}
 	}
 	
+	
+	@EventHandler
+	public void onPlayerClickSilverfish(PlayerInteractEntityEvent e) 
+	{
+		Entity ent = e.getRightClicked();
+		if (ent.getType().equals(EntityType.SILVERFISH)) {
+			Player p = e.getPlayer();
+			ent.addPassenger(p);
+		}
+	}
+	
+	@EventHandler
+	public void onDropLead(EntityDropItemEvent e) 
+	{
+		Item item = e.getItemDrop();
+		Entity ent = e.getEntity();
+		if (item.getItemStack().getType().equals(Material.LEAD)&&ent.getType().equals(EntityType.SILVERFISH)) {
+			e.setCancelled(true);
+		}
+	}
 	
 }
