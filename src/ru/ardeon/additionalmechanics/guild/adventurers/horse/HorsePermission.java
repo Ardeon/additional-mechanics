39201a796d2ab5df436940b89ae1ce74f4c75f8b
Builder <89637210314@mail.ru>
2020-09-13 19:01:18 +0300

    fix horsepermission

--- src/ru/ardeon/additionalmechanics/guild/adventurers/horse/HorsePermission.java
+++ src/ru/ardeon/additionalmechanics/guild/adventurers/horse/HorsePermission.java
@@ -7,17 +7,17 @@ import ru.ardeon.additionalmechanics.AdditionalMechanics;
 public class HorsePermission {
 	public static double getHp(Player p) {
 		int i = 0;
-		if (p.hasPermission("guilds.adventurers.horse.hp.1"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.hp.1"))
 			i=1;
-		if (p.hasPermission("guilds.adventurers.horse.hp.2"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.hp.2"))
 			i=2;
-		if (p.hasPermission("guilds.adventurers.horse.hp.3"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.hp.3"))
 			i=3;
-		if (p.hasPermission("guilds.adventurers.horse.hp.4"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.hp.4"))
 			i=4;
-		if (p.hasPermission("guilds.adventurers.horse.hp.5"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.hp.5"))
 			i=5;
-		if (p.hasPermission("guilds.adventurers.horse.hp.6"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.hp.6"))
 			i=6;
 		double hp = AdditionalMechanics.getPlugin().configLoader.getConfig().getDouble("guilds.adventurers.horse.hp."+i, 9);
 		return hp;
@@ -25,25 +25,25 @@ public class HorsePermission {
 	
 	public static double getSpeed(Player p) {
 		int i = 0;
-		if (p.hasPermission("guilds.adventurers.horse.speed.1"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.speed.1"))
 			i=1;
-		if (p.hasPermission("guilds.adventurers.horse.speed.2"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.speed.2"))
 			i=2;
-		if (p.hasPermission("guilds.adventurers.horse.speed.3"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.speed.3"))
 			i=3;
-		if (p.hasPermission("guilds.adventurers.horse.speed.4"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.speed.4"))
 			i=4;
-		if (p.hasPermission("guilds.adventurers.horse.speed.5"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.speed.5"))
 			i=5;
-		if (p.hasPermission("guilds.adventurers.horse.speed.6"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.speed.6"))
 			i=6;
-		if (p.hasPermission("guilds.adventurers.horse.speed.7"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.speed.7"))
 			i=7;
-		if (p.hasPermission("guilds.adventurers.horse.speed.8"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.speed.8"))
 			i=8;
-		if (p.hasPermission("guilds.adventurers.horse.speed.9"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.speed.9"))
 			i=9;
-		if (p.hasPermission("guilds.adventurers.horse.speed.10"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.speed.10"))
 			i=10;
 		double speed = AdditionalMechanics.getPlugin().configLoader.getConfig().getDouble("guilds.adventurers.horse.speed."+i, 0.2);
 		return speed;
@@ -51,32 +51,32 @@ public class HorsePermission {
 	
 	public static double getJump(Player p) {
 		int i = 0;
-		if (p.hasPermission("guilds.adventurers.horse.jump.1"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.jump.1"))
 			i=1;
-		if (p.hasPermission("guilds.adventurers.horse.jump.2"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.jump.2"))
 			i=2;
-		if (p.hasPermission("guilds.adventurers.horse.jump.3"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.jump.3"))
 			i=3;
-		if (p.hasPermission("guilds.adventurers.horse.jump.4"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.jump.4"))
 			i=4;
-		if (p.hasPermission("guilds.adventurers.horse.jump.5"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.jump.5"))
 			i=5;
-		if (p.hasPermission("guilds.adventurers.horse.jump.6"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.jump.6"))
 			i=6;
-		if (p.hasPermission("guilds.adventurers.horse.jump.7"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.jump.7"))
 			i=7;
-		if (p.hasPermission("guilds.adventurers.horse.jump.8"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.jump.8"))
 			i=8;
-		if (p.hasPermission("guilds.adventurers.horse.jump.9"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.jump.9"))
 			i=9;
-		if (p.hasPermission("guilds.adventurers.horse.jump.10"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.jump.10"))
 			i=10;
 		double jump = AdditionalMechanics.getPlugin().configLoader.getConfig().getDouble("guilds.adventurers.horse.jump."+i, 0.6);
 		return jump;
 	}
 	
 	public static boolean getCanUse(Player p) {
-		if (p.hasPermission("guilds.adventurers.horse.use"))
+		if (p.hasPermission("additionalmechanics.adventurer.horse.use"))
 			return true;
 		return false;
 	}
