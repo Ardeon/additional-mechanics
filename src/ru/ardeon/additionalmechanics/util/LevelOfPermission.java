7a9aa53c41e2ebc84d46acc6382dda36475e5d3c
Builder <89637210314@mail.ru>
2020-10-04 16:53:20 +0300

    fix levelOfPermission util

--- src/ru/ardeon/additionalmechanics/util/LevelOfPermission.java
+++ src/ru/ardeon/additionalmechanics/util/LevelOfPermission.java
@@ -2,13 +2,17 @@ package ru.ardeon.additionalmechanics.util;
 
 import org.bukkit.entity.Player;
 
+import ru.ardeon.additionalmechanics.AdditionalMechanics;
+
 public class LevelOfPermission {
 	public static int getLevel(Player p, String perm, int max) {
 		int l=0;
 		for(int i = 0; i < max; i++) {
-			if (p.hasPermission(perm+"\\."+i)) 
+			if (p.isPermissionSet(perm+'.'+i)&&p.hasPermission(perm+'.'+i)) {
 				l=i;
+			}
 		}
+		//AdditionalMechanics.getPlugin().getLogger().info(""+l);
 		return l;
 	}
 }
