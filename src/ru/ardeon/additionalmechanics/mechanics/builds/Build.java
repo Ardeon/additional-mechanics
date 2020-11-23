c347e64b4f9250891a6c778811420cdd0fe950d2
Builder <89637210314@mail.ru>
2020-11-23 12:34:02 +0300

    Builds

---
+++ src/ru/ardeon/additionalmechanics/mechanics/builds/Build.java
@@ -0,0 +1,27 @@
+package ru.ardeon.additionalmechanics.mechanics.builds;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+
+public class Build {
+	Material[][][] blocks;//[x] [y] [z] = new Material[1][1][1]
+	
+	
+	public Build newBuild(Location loc, String name) {
+		HashMap<String, Material> map = new HashMap<String, Material>();
+		
+		return null;
+	}
+	
+	public Build(String name) {
+		
+	}
+	
+	public List<Build> loadAll(){
+		
+		return null;
+	}
+}
